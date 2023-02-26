6b791e80dafcb5b5bec335e48544d91f2b4e9f26
AxelBaranowski3il <baranowa@3il.fr>
2023-02-26 14:03:16 +0100

    test resFibo => borne = 20

--- FiboTest.java
+++ FiboTest.java
@@ -1,7 +1,26 @@
 import static org.junit.Assert.*;
 
 public class FiboTest {
-
+    String res20 = "1\n" +
+            "1\n" +
+            "2\n" +
+            "3\n" +
+            "5\n" +
+            "8\n" +
+            "13\n" +
+            "21\n" +
+            "34\n" +
+            "55\n" +
+            "89\n" +
+            "144\n" +
+            "233\n" +
+            "377\n" +
+            "610\n" +
+            "987\n" +
+            "1597\n" +
+            "2584\n" +
+            "4181\n" +
+            "6765\n";
     @org.junit.Test
     public void testResFibo1() {
         assertEquals("1\n", Fibo.resFibo(1));
@@ -10,5 +29,10 @@ public class FiboTest {
     @org.junit.Test
     public void testResFibo2() {
         assertEquals("1\n1\n", Fibo.resFibo(2));
+    }
+
+    @org.junit.Test
+    public void testResFibo20() {
+        assertEquals(res20, Fibo.resFibo(20));
     }
 }
